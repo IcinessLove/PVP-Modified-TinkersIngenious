68c11daf279c59bd9104026653f72cefe9187882
IcinessLove <sizhelius.li@mail.utoronto.ca>
2025-08-26 23:37:32 +0800

    fixed the bug of KnightBloodline

--- src/main/java/com/xiaoyue/tinkers_ingenuity/content/modifier/defense/KnightBloodline.java
+++ src/main/java/com/xiaoyue/tinkers_ingenuity/content/modifier/defense/KnightBloodline.java
@@ -1,37 +1,14 @@
 package com.xiaoyue.tinkers_ingenuity.content.modifier.defense;
 
-import com.xiaoyue.tinkers_ingenuity.TinkersIngenuity;
-import com.xiaoyue.tinkers_ingenuity.content.generic.ISimpleModule;
-import com.xiaoyue.tinkers_ingenuity.content.generic.SerialLoader;
-import dev.xkmc.l2library.init.events.GeneralEventHandler;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
 import com.xiaoyue.tinkers_ingenuity.content.generic.SimpleModifier;
-import slimeknights.mantle.data.loadable.primitive.FloatLoadable;
-import slimeknights.mantle.data.loadable.primitive.IntLoadable;
-import slimeknights.mantle.data.loadable.record.RecordLoadable;
-import slimeknights.mantle.data.registry.GenericLoaderRegistry;
-import slimeknights.tconstruct.library.modifiers.ModifierEntry;
-import slimeknights.tconstruct.library.modifiers.ModifierHooks;
-import slimeknights.tconstruct.library.modifiers.hook.armor.ModifyDamageModifierHook;
-import slimeknights.tconstruct.library.modifiers.hook.armor.OnAttackedModifierHook;
-import slimeknights.tconstruct.library.module.HookProvider;
-import slimeknights.tconstruct.library.module.ModuleHook;
-import slimeknights.tconstruct.library.tools.context.EquipmentContext;
-import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
-import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 import net.minecraft.world.entity.LivingEntity;
-import slimeknights.tconstruct.library.tools.context.EquipmentChangeContext;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import slimeknights.tconstruct.TConstruct;
-import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.modules.technical.ArmorLevelModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
-
-import java.util.List;
+import slimeknights.tconstruct.library.modifiers.modules.technical.ArmorLevelModule;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 
 public class KnightBloodline extends SimpleModifier {
 
@@ -39,21 +16,24 @@ public class KnightBloodline extends SimpleModifier {
 
     public KnightBloodline(){
         super();
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST,KnightBloodline::onHurt);
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, KnightBloodline::onDamage);
     }
 
     @Override
     public void registerHooks(ModuleHookMap.Builder hookBuilder) {
         hookBuilder.addModule(new ArmorLevelModule(KBL, false, null));
     }
-    public static void onHurt(LivingHurtEvent event) {
+
+    public static void onDamage(LivingDamageEvent event) {
         LivingEntity living = event.getEntity();
         living.getCapability(TinkerDataCapability.CAPABILITY).ifPresent((holder) -> {
             int levels = holder.get(KBL, 0);
             float amount = event.getAmount();
             if (levels > 0 && amount > 0) {
-                float reduced = living.getMaxHealth() / (Math.min(5,1 + 0.4f * levels));
-                event.setAmount(Math.min(amount,reduced));
+                // 计算最大允许伤害：最大生命值除以系数（最小为5）
+                float maxDamage = living.getMaxHealth() / Math.min(5, 1 + 0.4f * levels);
+                // 限制最终伤害
+                event.setAmount(Math.min(amount, maxDamage));
             }
         }); 
     }
